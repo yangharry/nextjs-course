db7de5dfe9d2c8feb72b3e50a8d69dc82b860142
yangharry <myid7771@naver.com>
2023-07-07 20:17:43 +0900

    head and meta tag

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,17 @@
 import EventList from '@/components/events/event-list';
 import { getFeaturedEvents } from '@/helpers/api-util';
 
+import Head from 'next/head';
+
 export default function HomePage(props) {
   // const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
