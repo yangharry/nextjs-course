e7dccf5cbf308ac3c7744bcf843c7e19aedd1125
yangharry <myid7771@naver.com>
2023-07-11 21:32:52 +0900

    section8 start

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
-import Layout from '@/components/layout/layout';
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
-import Head from 'next/head';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="NextJS Events" />
-        <meta name="viewport" content="inital-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,9 @@
-// import { getFeaturedEvents } from '@/dummy-data';
-import EventList from '@/components/events/event-list';
-import { getFeaturedEvents } from '@/helpers/api-util';
-
-import Head from 'next/head';
-
-export default function HomePage(props) {
-  // const featuredEvents = getFeaturedEvents();
-
+function HomePage() {
   return (
     <div>
-      <Head>
-        <title>NextJS Events</title>
-        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
-      </Head>
-      <EventList items={props.events} />
+      <h1>The Home Page</h1>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
-
-  return {
-    props: {
-      events: featuredEvents,
-    },
-    revalidate: 3600,
-  };
-}
+export default HomePage;
