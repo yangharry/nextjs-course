d0a21576a3ce9cefa11ee3fff1b0732c78750187
yangharry <myid7771@naver.com>
2023-07-26 03:34:59 +0900

    section9 set

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
-// import { getFeaturedEvents } from '@/dummy-data';
-import EventList from '@/components/events/event-list';
-import { getFeaturedEvents } from '@/helpers/api-util';
-
 import Head from 'next/head';
 
-export default function HomePage(props) {
-  // const featuredEvents = getFeaturedEvents();
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
+function HomePage(props) {
   return (
     <div>
       <Head>
         <title>NextJS Events</title>
         <meta name="description" content="Find a lot of great events that allow you to evolve..." />
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events} />
     </div>
   );
@@ -25,6 +24,8 @@ export async function getStaticProps() {
     props: {
       events: featuredEvents,
     },
-    revalidate: 3600,
+    revalidate: 1800,
   };
 }
+
+export default HomePage;
