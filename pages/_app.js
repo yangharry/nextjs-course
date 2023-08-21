322ef07a679decfe38c77a22b8d1c169e3454c11
yangharry <myid7771@naver.com>
2023-08-22 03:21:41 +0900

    section11 start

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,7 @@
-import Layout from '@/components/layout/layout';
-// import Notification from '@/components/ui/notification';
-import { NotificationContextProvider } from '@/store/notification-context';
-import '@/styles/globals.css';
+import '../styles/globals.css';
 
-import Head from 'next/head';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <NotificationContextProvider>
-      <Layout>
-        <Head>
-          <title>Next Events</title>
-          <meta name="description" content="NextJS Events" />
-          <meta name="viewport" content="inital-scale=1.0, width=device-width" />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-    </NotificationContextProvider>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
