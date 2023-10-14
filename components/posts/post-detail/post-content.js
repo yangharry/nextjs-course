f1ee02d1456f7dc13c2136363db4401d4a59da97
yangharry <myid7771@naver.com>
2023-10-14 21:55:51 +0900

    section11 markdown

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,26 @@
+import ReactMarkdown from 'react-markdown';
+
+import PostHeader from './post-header';
+
+import classes from './post-content.module.css';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs',
+  title: 'Getting Started with NextJS',
+  image: 'getting-started-nextjs.png',
+  date: '2022-02-10',
+  content: '# This is a first post',
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
+import { Fragment } from 'react';
+
 import FeaturedPosts from '@/components/home-page/featered-posts';
 import Hero from '@/components/home-page/hero';
-import { Fragment } from 'react';
 
 const DUMMY_POSTS = [
   {
     slug: 'getting-started-with-nextjs',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes bulding fullstack react apps and sites a breeze and ships with built-in SSR.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes bulding fullstack react apps and sites a breeze and ships with built-in SSR.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'NextJS is a the React framework for production - it makes bulding fullstack react apps and sites a breeze and ships with built-in SSR.',
+    date: '2022-02-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
     title: 'Getting Started with NextJS',
     image: 'getting-started-nextjs.png',
     excerpt:
